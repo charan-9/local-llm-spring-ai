9c6edb45cbbd4787f64bb860283b0458cee6dea8
charan sai <charansai3664@gmail.com>
2026-06-01 22:35:12 +0530

    Added new query to fetch only 10 queries

--- src/main/java/com/local/project/local_llm_spring_ai/repository/ChatMessageRepository.java
+++ src/main/java/com/local/project/local_llm_spring_ai/repository/ChatMessageRepository.java
@@ -3,12 +3,18 @@ package com.local.project.local_llm_spring_ai.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.local.project.local_llm_spring_ai.entity.ChatMessage;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    List<ChatMessage> findBySessionIdOrderByCreatedAtAsc(String SesstionId);
+    List<ChatMessage> findBySessionIdOrderByCreatedAtAsc(String SessionId);
+
+    @Query("""
+            SELECT c FROM ChatMessage c WHERE c.sessionId = :sessionId ORDER BY c.createdAt DESC LIMIT 10
+            """)
+    List<ChatMessage> findRecentMessages(String SessionId);
 
 }
